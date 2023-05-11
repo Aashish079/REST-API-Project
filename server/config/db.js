07ed3b1d6651a3c31ac5f68fa1d4fef13d3a10ee
Aashish Karki <106550372+Aashish079@users.noreply.github.com>
2023-05-11 17:45:47 +0545

    sm-change

--- server/config/db.js
+++ server/config/db.js
@@ -2,7 +2,7 @@ require("dotenv").config();
 const mongoose = require('mongoose');
 
 const connecttoDB = async ()=>{
-    mongoose.connect("process.env.MONGO_KEY")
+    mongoose.connect(process.env.MONGO_KEY)
     .then((conn)=>{
         console.log("DB Connected");
     })
