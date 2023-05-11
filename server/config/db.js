4786257617a0f8331a16780b094f80ec78dbf5f2
Aashish Karki <106550372+Aashish079@users.noreply.github.com>
2023-05-11 17:44:04 +0545

    secured api key in a .env file

--- server/config/db.js
+++ server/config/db.js
@@ -2,7 +2,7 @@ require("dotenv").config();
 const mongoose = require('mongoose');
 
 const connecttoDB = async ()=>{
-    mongoose.connect("mongodb+srv://developeraashish079:vinayak10@cluster0.0an5h0k.mongodb.net/test")
+    mongoose.connect("process.env.MONGO_KEY")
     .then((conn)=>{
         console.log("DB Connected");
     })
